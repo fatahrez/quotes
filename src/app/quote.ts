3ce2507d3c3ba2aa7e0dba99cabfebfa06e201b8
Abdulfatah Mohamed <fertahrez@gmail.com>
2018-09-16 17:37:55 +0300

    add material button design

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,5 +2,7 @@ export class Quote {
   public showQuote: boolean;
   constructor(public author: string, public submittor: string, public theQuote: string, public upVote: number, public downVote: number) {
     this.showQuote = false;
+    this.upVote = 0;
+    this.downVote = 0;
   }
 }

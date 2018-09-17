d12f69acdc36e5f2165a97deaedd8dc2dca9c23c
Abdulfatah Mohamed <fertahrez@gmail.com>
2018-09-17 11:04:34 +0300

    add appHighlight to input in highlight.directive.ts

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef, Input, HostListener } from '@angular/core';
 export class HighlightDirective {
   constructor(private elem: ElementRef) {}
   @Input() defaultColor: string;
-  @Input('appHighlight') highlightColor: string;
+  @Input('appHighlight') highlightColor:  string;
 
   @HostListener('mouseenter') onMouseEnter() {
     this.highlight (this.highlightColor || this.defaultColor || 'yellow');

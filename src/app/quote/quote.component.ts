f6193c9eea9a9d29d0ff937ec2219034cc9a997c
Abdulfatah Mohamed <fertahrez@gmail.com>
2018-09-17 10:52:28 +0300

    change quote 1 contents

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,8 +9,8 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   color: string;
   quotes = [
-    new Quote('Abdul', 'yeye', 'Hey thea', 0, 0),
-    new Quote('Fatah', 'mwenye', 'We cool', 0, 0)
+    new Quote('Zlatan', 'Fatah', 'Genius always finds a way', 0, 0),
+    new Quote('Drake', 'Abdul', 'Started from the bottom now we here', 0, 0)
   ];
 
   get addNewQuoteFunc() {
